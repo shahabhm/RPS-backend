b589185f7eed1a851368539d4dbd4def5ceb1851
shahab <shhm3834@gmail.com>
2025-04-21 21:28:46 +0330

    Add get, start, and finish meeting APIs

--- handlers/Meeting.ts
+++ handlers/Meeting.ts
@@ -1,44 +1,58 @@
 import {body} from "express-validator";
 import {authenticateToken, ICustomRequest, validateAPI} from "../Middlewares";
 import {NextFunction, Response, Router} from "express";
+import {addPrescription, finishMeeting, getMeeting, getMeetings, startMeeting} from "../ts_handlers";
 
 const router = Router();
 
-router.post('/api/v1/doctor/add_prescription',
-    body('patient_id').notEmpty(),
-    body('doctor_id').notEmpty(),
+router.post('/api/v1/meeting/prescription',
+    body('meetingId').isString,
     body('medicines').isArray(),
-    body('note').notEmpty(),
+    body('note').isString(),
     validateAPI,
     async (req: ICustomRequest, res: Response, next: NextFunction) => {
         try {
-            const { patient_id, doctor_id, medicines, note } = req.body;
-            const response = await addPrescription(patient_id, doctor_id, medicines, note);
+            const {meetingId, medicines, note} = req.body;
+            const response = await addPrescription(meetingId, medicines, note);
             res.send(response);
         } catch (e) {
-            next (e);
+            next(e);
         }
     }
 );
-
-// app.get('/api/v1/doctor/meetings',
-//      authenticateToken,
-//     get_account_by_id,
-//     async (req, res) => {
-//         const { page, limit } = req.query;
-//         console.log(page, limit);
-//         const response = await handlers.get_doctor_meetings(req.account.doctor_id.toString());
-//         res.send({items: response});
-//     });
 
 // returns the list of meetings
 router.get('/api/v1/meetings',
     authenticateToken,
     async (req: ICustomRequest, res: Response, next: NextFunction) => {
-        const { page, limit } = req.query;
+        const {page, limit} = req.query;
         console.log(page, limit);
-        const response = await getDoctorMeetings(req.user.doctor_id.toString());
+        const response = await getMeetings(req.user.patient_id, req.user.doctor_id);
         res.send({items: response});
+    });
+
+router.get('/api/v1/meeting',
+    authenticateToken,
+    async (req: ICustomRequest, res: Response, next: NextFunction) => {
+        const {meetingId} = req.query;
+        const meeting = await getMeeting(meetingId as string);
+        res.send(meeting);
+    });
+
+router.post('/api/v1/meeting/start',
+    authenticateToken,
+    async (req: ICustomRequest, res: Response, next: NextFunction) => {
+        const {meetingId} = req.body;
+        const meeting = await startMeeting(meetingId);
+        res.send(meeting);
+    });
+
+router.post('/api/v1/meeting/finish',
+    authenticateToken,
+    async (req: ICustomRequest, res: Response, next: NextFunction) => {
+        const {meetingId} = req.body;
+        const meeting = await finishMeeting(meetingId);
+        res.send(meeting);
     });
 
 
